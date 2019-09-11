87d92922531ba42793c6a4673a42bfb07af0fd55
DESKTOP-0C7AONI\Tun <tminhein@gmail.com>
2019-09-11 22:58:11 +0100

    updated admin name

--- Aks/Modules/AKS/Dashboard/service-account/bearer-token.sh
+++ Aks/Modules/AKS/Dashboard/service-account/bearer-token.sh
@@ -1,1 +1,1 @@
-kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep dixi-admin | awk '{print $1}')
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep pngo-admin | awk '{print $1}')
